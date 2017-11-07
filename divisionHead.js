07c08ae31b2bf6aae99529a1858e767a6896ab1d
9Kevinzhao <9kzhao@stpatrick.org>
2017-11-07 14:39:31 -0600

    Add files via upload

---
+++ divisionHead.js
@@ -0,0 +1,10 @@
+const Random = require('./Random.js');
+const divisionHead = function(){
+	let id = "d-"+Random().digit(8);
+	let managers=Random().discreteRangeIn(1,10);
+	let workers=Random().discreteRangeIn(20,300);
+	let baseSalary=Random().discreteRangeIn(2,5)*50000;
+	function pay(){
+	return (managers*10000+workers*50)*Math.log(workers);
+}	
+}

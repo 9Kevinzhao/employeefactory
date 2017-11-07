620a693414739c55432f0a4af6978627d1c812d2
9Kevinzhao <9kzhao@stpatrick.org>
2017-11-07 14:33:04 -0600

    Update manager.js

--- manager.js
+++ manager.js
@@ -1,1 +1,15 @@
+const Random = require('./Random.js');
 let baseSalary = (Random().discreteRangeIn(4,9)*10000);
+let workers= Random().discreteRangeIn(4,20);
+let array = [];
+function pay(){
+  for(let i=0;i<workers;i++){
+    while(i<=10){
+    array.push($250)
+    }
+    else{
+      array.push($400)
+    }
+  }
+}
+    

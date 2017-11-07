9552d8939b330019ee8dfd9d5d84895aedab0a15
9Kevinzhao <9kzhao@stpatrick.org>
2017-11-07 14:43:06 -0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,10 +3,11 @@ const Random = require('./Random.js');
 const worker = function(){
   let id = "w-"+Random().digit(8);
   let workers = Random().discreteRangeIn(4,20);;
-  let baseSalary = (Random().discreteRangeIn(4,7)*5);
+  let hours = (Random().discreteRangeIn(4,7)*5);
 
   function pay(){
-
+    let array=[dailypay,weeklypay,yearlypay]
+   
   }
 return{id,workers,baseSalary}
 }
